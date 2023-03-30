59edb322c243e2d9e1f638f45b723a062e56734b
Efrat Leibenzon <129198213+EfratLeibenzon@users.noreply.github.com>
2023-03-30 17:01:17 +0300

    third upload

--- js/main.js
+++ js/main.js
@@ -44,83 +44,56 @@ function buildBoard() {
             }
         }
     }
+    placeMines(gLevel.MINES)
 
-    console.table(gBoard)
-    var poss = findEmptycellsByAmount(gLevel.MINES)
-    placeMines(poss)
+    for (var i = 0; i < gBoard.length; i++) {
+        for (var j = 0; j < gBoard[i].length; j++) {
+            gBoard[i][j].minesAroundCount = setMinesNegsCount(i, j, gBoard)
+
+        }
+
+
+    }
     console.table(gBoard)
 }
 
 
+function placeMines(amount) {
 
+    for (var k = 0; k < amount; k++) {
+        var emptyCells = findEmptyPoss()
 
+        var cellPos = emptyCells[getRandomInt(0, emptyCells.length)]
+        var currCell = gBoard[cellPos.i][cellPos.j]
+        currCell.isMine = true
 
-
-
-function placeMines(emptyPoss) {
-    for (var i = 0; i < gBoard.length; i++) {
-        for (var j = 0; j < gBoard[i].length; j++) {
-            if (gBoard[i][j] === emptyPoss.pop()) { gBoard[i][j].isMine = true }
-            else continue
-
-        }
     }
 }
 
+function getCell() {
+    var row = getRandomInt(0, gBoard.length)
+    var col = getRandomInt(0, gBoard[0].length)
+    var cell = gBoard[row][col]
+    return cell
+}
 
-function findEmptycellsByAmount(amount) {
-    var emptyCellsByAmount = []
-    var emptyCells = []
-    if (amount > gBoard.length * gBoard.length) return
+function findEmptyPoss() {
+
+    var emptyPoss = []
+
     for (var i = 0; i < gBoard.length; i++) {
         for (var j = 0; j < gBoard[i].length; j++) {
             var cell = gBoard[i][j]
-            if (cell.isMine) continue
-            var pos = { i, j }
-            emptyCells.push(pos)
+            if (!cell.isMine) {
+                var pos = { i, j }
+                emptyPoss.push(pos)
+            }
         }
     }
-
-    for (var i = 0; i < amount; i++) {
-        for (var j = 0; j < emptyCells; j++) {
-            var mineCell = emptyCells.splice(getRandomInt(0, emptyCells.length), 1)[0]
-            emptyCellsByAmount.push(mineCell)
-        }
-
-    }
-    console.log(emptyCellsByAmount)
-    return emptyCellsByAmount
+    return emptyPoss
 }
 
 
-
-
-// function getRandomEmptyCells(mat) {
-//     var randEmptyCells = []
-//     for (var i = 0; i < mat.length; i++) {
-//         for (var j = 0; j < mat[i].length; j++) {
-//             var row = getRandomInt(0, mat.length)
-//             var col = getRandomInt(0, mat[i].length)
-//             if (gBoard[row][col].isMine === true) {
-//                 continue
-//             } else {
-//                 randEmptyCells.push(gBoard[row][col])
-
-//             }
-//         }
-//     }
-
-//     return randEmptyCells
-// }
-
-
-
-
-
-
-// Count mines around each cell
-// and set the cell's
-// minesAroundCount.
 
 function setMinesNegsCount(rowIdx, colIdx, mat) {
     var negsCount = 0
@@ -137,22 +110,6 @@ function setMinesNegsCount(rowIdx, colIdx, mat) {
     return negsCount
 }
 
-
-// function setMinesNegsCount(board) {
-//     var negsCount = 0
-//     for (var i = board.length - 1; i <= board.length + 1; i++) {
-//         if (i < 0 || i >= board.length) continue
-
-//         for (var j = board[i].length - 1; j <= board[i].length + 1; j++) {
-//             if (j < 0 || j >= board[i].length) continue
-//             if (i === board.length && j === board[i].length) continue
-//             if (board[i][j].isMine === true) negsCount++
-//             gBoard[i][j].minesAroundCount = negsCount
-//         }
-//     }
-// }
-
-
 // Render the board as a <table>
 // to the page
 
@@ -164,21 +121,34 @@ function renderBoard(mat, selector) {
         strHTML += '<tr>'
         for (var j = 0; j < mat[0].length; j++) {
             var cell = mat[i][j]
-            cell = (cell.isMine) ? MINE : EMPTY
-            const className = `cell cell-${i}-${j}`
-            strHTML += `<td class="${className} onclick="onCellClicked(this, ${i}, ${j})">${cell}</td>\n`
+            var numOfCellNegs = cell.minesAroundCount
+            if (cell.isMine) { cell = MINE }
+            else if (cell.isMine === false && numOfCellNegs > 0) { cell = numOfCellNegs }
+            else { cell = EMPTY }
+            const className = `cell cell-${i}-${j} covered`
+            strHTML += `<td class="${className}" onclick="onCellClicked(this, ${i}, ${j})">${cell}</td>\n`
         }
         strHTML += '</tr>\n'
+
     }
     strHTML += '</tbody></table>'
 
     const elContainer = document.querySelector(selector)
     elContainer.innerHTML = strHTML
+
 }
 
 // Called when a cell is clicked
 function onCellClicked(elCell, i, j) {
-    // gBoard[i][j].isShown : true
+    gBoard[i][j].isShown = true
+    console.log('i', i, 'j', j)
+    elCell.classList.remove('covered')
+    if (gBoard[i][j].isMine === true) {
+        console.log('game over')
+    } else if (gBoard[i][j].isMine === EMPTY) {
+        elCell.classList.remove('covered')
+
+    }
 }
 
 
@@ -194,6 +164,7 @@ function onCellMarked(elCell) {
 // marked, and all the other cells
 // are shown
 function checkGameOver() {
+
 
 }
 
@@ -215,11 +186,31 @@ function expandShown(board, elCell, i, j) {
 }
 
 
+// function startTimer() {
+//     gStartTime = Date.now();
+//     gIntervalId = setInterval(updateTimer, 10);
+//     // playSound('count');
+//   }
+
+//   function stopTimer() {
+//     clearInterval(gIntervalId);
+//   }
+
+function renderCell(location, value) {
+    const cellSelector = '.' + getClassName(location) // cell-i-j
+    const elCell = document.querySelector(cellSelector)
+    elCell.innerHTML = value
+}
+
+// Returns the class name for a specific cell
+function getClassName(location) {
+    const cellClass = 'cell-' + location.i + '-' + location.j
+    return cellClass
+}
+
 function getRandomInt(min, max) {
     min = Math.ceil(min)
     max = Math.floor(max)
     return Math.floor(Math.random() * (max - min) + min)
 }
-function drawNum(NUMS) {
-    return NUMS.splice(getRandomInt(0, NUMS.length), 1)[0]
-}
+
